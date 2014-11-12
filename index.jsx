0e73d23a9f28656aa51aac6b84565dc15fd2e11d
James Harris <wizardofmath@gmail.com>
2014-11-11 18:00:46 -0800

    Refactored index.jsk

--- index.jsx
+++ index.jsx
@@ -1,29 +1,34 @@
-var TetrisGame,
-    GameBox,
-    GamePieces,
-    Piece,
-    PieceCell,
-    PreviewPane,
-    GameControls,
-    PlayBtn,
-    SaveBtn,
-    PauseBtnl,
-    ScorePane;
+var GameBox = React.createClass({
+    getInitialState: function(){
+        return {gameState:this.props.gameState};
+    },
+    render: function() {
+        return (
+            <div className="GameBox">{this.state.gameState.gameOver +" "}
+            {this.state.gameState.board().map(function(row) {
+                return <div className="row">{row.map(function(cell) {
+                    var cellState= cell.occupied ? "occupied" : "free";
+                    return <div className={"cell"} style={{"backgroundColor": cell.color}}></div>;
+            })}</div>;
+            })}
+            {console.log(this.state.gameState.cells)}
+            </div>
+        )
+    }
+});
 
 
 var TetrisGame = React.createClass({
   render: function() {
     return (
       <div className="TetrisGame">
-        <GameBox/>
-        <PreviewPane/>
-        <GameControls/>
+        <GameBox gameState={this.props.gameState}/>
       </div>
     );
   }
 });
 
 React.render(
-  <TetrisGame/>,
+  <TetrisGame gameState={gameA}/>,
   document.getElementById('main_Container')
 );
