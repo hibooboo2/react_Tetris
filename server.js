c1bbb9ccba0f0d5eccf62d927b0aeee976c69b7f
James Harris <wizardofmath@gmail.com>
2014-12-02 11:20:56 -0700

    Added mongo And changed chat a little.

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
 var express = require('express'),
-    app = express();
-server = require('http').createServer(app),
-io = require('socket.io').listen(server);
-var serverPort = process.env.PORT ? process.env.PORT : 3000;
-server.listen(serverPort);
-console.log("server started port: " + serverPort);
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(process.env.PORT ? process.env.PORT : 3000);
+
 app.use('/', express.static(__dirname + '/app'));
 var currentUsers = {};
+
 io.sockets.on('connection', function (socket) {
-    console.log(socket.id + " Joined");
+
     socket.on('login', function (data) {
         currentUsers["user_" + data.name] = socket.id;
-        socket.name = data.name;
-        console.log(currentUsers);
+        console.log("Logged IN! " + data.name);
     });
-    socket.on('send message', function (data) {
-        data.message = data.from + " (" + new Date().toLocaleTimeString() + "): " + data.message;
-        io.emit('new message', data);
-        console.log(data);
-        console.log(socket.name + " Said");
+
+    socket.on('send_message', function (data) {
+        data.timeStamp = " (" + new Date().toLocaleTimeString() + ") ";
+        console.log("Send message " + data);
+        io.emit('new_message', data);
     });
+
     socket.on('recieved', function (data) {
         socket.to(currentUsers["user_" + data.from]).emit('new message', data);
     });
+
     socket.on('whisper', function (data) {
-        data.message = data.from + " Whispered (" + new Date().toLocaleTimeString() + "): " + data.message;
-        socket.to(currentUsers["user_" + data.to]).emit('new message', data);
+        data.timeStamp = " (" + new Date().toLocaleTimeString() + ") ";
+        data.message = " Whispered " + data.message;
+        socket.to(currentUsers["user_" + data.to]).emit('whispered_message', data);
+        console.log(socket.id + " Said " + data.timeStamp + data.message + " To " + data.to + " AKA " + currentUsers["user_" + data.to]);
     });
+
     socket.on('disconnect', function (data) {
-        console.log(data);
         console.log(socket.name + " Left");
         currentUsers["user_" + socket.name] = undefined;
+        console.log(data);
     });
+
     socket.on('game', function (data) {
         console.log(data);
     });
