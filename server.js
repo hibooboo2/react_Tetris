1c43e2daeae2e3d067d670add2701590951921e1
James Harris <wizardofmath@gmail.com>
2014-12-01 18:24:28 -0700

    Changed to new chat box.

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express'),
     app = express();
 server = require('http').createServer(app),
 io = require('socket.io').listen(server);
-
-server.listen(process.env.PORT || 3000);
-
+var serverPort = process.env.PORT ? process.env.PORT : 3000;
+server.listen(serverPort);
+console.log("server started port: " + serverPort);
 app.use('/', express.static(__dirname + '/app'));
 var currentUsers = {};
 io.sockets.on('connection', function (socket) {
@@ -15,17 +15,17 @@ io.sockets.on('connection', function (socket) {
         console.log(currentUsers);
     });
     socket.on('send message', function (data) {
-        var spltMessage = "Failed to send a message."
-        if (data.message !== undefined) {
-            spltMessage = data.message.split(":");
-        }
-        if (spltMessage[0] === "/w") {
-            socket.to(currentUsers["user_" + spltMessage[1]]).emit('new message', data.name + " Whispered: " + " (" + new Date().toLocaleTimeString() + "): " + spltMessage[2]);
-        } else {
-            io.emit('new message', data.name + " (" + new Date().toLocaleTimeString() + "): " + data.message);
-            console.log(data);
-            console.log(socket.name + " Said");
-        }
+        data.message = data.from + " (" + new Date().toLocaleTimeString() + "): " + data.message;
+        io.emit('new message', data);
+        console.log(data);
+        console.log(socket.name + " Said");
+    });
+    socket.on('recieved', function (data) {
+        socket.to(currentUsers["user_" + data.from]).emit('new message', data);
+    });
+    socket.on('whisper', function (data) {
+        data.message = data.from + " Whispered (" + new Date().toLocaleTimeString() + "): " + data.message;
+        socket.to(currentUsers["user_" + data.to]).emit('new message', data);
     });
     socket.on('disconnect', function (data) {
         console.log(data);
