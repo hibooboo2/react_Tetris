b0d8ff077e7897dfb8591ad51df3a196c5982060
James Harris <wizardofmath@gmail.com>
2014-12-20 14:33:33 -0700

    Reverted. Will fix later

--- app/js/boardEngine.js
+++ app/js/boardEngine.js
@@ -148,10 +148,7 @@ var BoardEngine = function () {  // jshint ignore:line
             var previousHeld = this.heldPiece;
             this.heldPiece = this.fallingPiece.tetromino;
             if (previousHeld) {
-                this.fallingPiece = new this.pieceEngine.newPiece(previousHeld,{
-                    x: 3,
-                    y: 0
-                }, 0, 1);
+                this.fallingPiece = new this.pieceEngine.newPiece(previousHeld);
             } else {
                 this.newFallingPiece();
             }
